a7d6244c6da40f9e342549768ac5fe54bf5082d4
Ran Arino <ranarino0611ca@gmail.com>
2025-02-10 09:51:05 -0500

    Add LLMController to the module exports in utils

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,9 +1,11 @@
 # core/utils/__init__.py
 
 from .libs.huggingface import load_hf_dataset
+from .llm import LLMController
 from .path import get_project_root
 
 __all__ = [
     "load_hf_dataset",
-    "get_project_root"
+    "get_project_root",
+    "LLMController"
 ]
