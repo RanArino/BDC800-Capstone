daf89eec64c54ea079d3789e7a7b733d74c4999a
Ran Arino <ranarino0611ca@gmail.com>
2025-02-10 17:18:50 -0500

    Add __init__.py to rag_core and update exports; remove LLMController from utils

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,11 +1,9 @@
 # core/utils/__init__.py
 
 from .libs.huggingface import load_hf_dataset
-from .llm import LLMController
 from .path import get_project_root
 
 __all__ = [
     "load_hf_dataset",
     "get_project_root",
-    "LLMController"
 ]
