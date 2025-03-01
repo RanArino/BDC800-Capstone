da693edb7934b18e9f80ba3002badcf54d0c8c75
Ran Arino <ranarino0611ca@gmail.com>
2025-03-01 10:58:37 -0500

    feat `rag_core/__inti__`: add new functions into rag core module

--- core/rag_core/__init__.py
+++ core/rag_core/__init__.py
@@ -1,9 +1,14 @@
 # core/rag_core/__init__.py
 
 from .indexing.chunker import Chunker
+from .indexing.dim_reduction import run_dim_reduction
+from .indexing.clustering import run_clustering
 from .llm.controller import LLMController
 
 __all__ = [
     "Chunker",
-    "LLMController"
+    "LLMController",
+    "run_doc_summary",
+    "run_dim_reduction",
+    "run_clustering"
 ]
