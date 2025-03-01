2572acf4e3762f5defd6afddde17399589d4028e
Ran Arino <ranarino0611ca@gmail.com>
2025-03-01 16:48:25 -0500

    feat `rag_core/__init__.py`: add `reduce_query_embedding` to exports

--- core/rag_core/__init__.py
+++ core/rag_core/__init__.py
@@ -1,7 +1,7 @@
 # core/rag_core/__init__.py
 
 from .indexing.chunker import Chunker
-from .indexing.dim_reduction import run_dim_reduction
+from .indexing.dim_reduction import run_dim_reduction, reduce_query_embedding
 from .indexing.clustering import run_clustering
 from .llm.controller import LLMController
 
@@ -9,6 +9,7 @@ __all__ = [
     "Chunker",
     "LLMController",
     "run_doc_summary",
-    "run_dim_reduction",
     "run_clustering"
+    "run_dim_reduction",
+    "reduce_query_embedding"
 ]
