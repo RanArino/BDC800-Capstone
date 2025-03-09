4a2b44d7e8be064062ebe40253083dead05305c8
Ran Arino <ranarino0611ca@gmail.com>
2025-03-09 11:46:07 -0400

    feat `frameworks/__init__.py`: add ScalerV1RAG to exports

--- core/frameworks/__init__.py
+++ core/frameworks/__init__.py
@@ -3,10 +3,12 @@
 from .schema import RAGConfig, DatasetConfig, ChunkerConfig, RetrievalGenerationConfig, RAGResponse
 from .simple import SimpleRAG
 from .scaler import ScalerRAG
+from .scaler_v1 import ScalerV1RAG
 
 all = [
     "SimpleRAG",
     "ScalerRAG",
+    "ScalerV1RAG",
     "RAGConfig",
     "DatasetConfig",
     "ChunkerConfig",
